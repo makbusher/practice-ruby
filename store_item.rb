25117cea9ae81b6c9b3974cbc3e675c75fe71683
Makayla <makaylabusher@gmail.com>
2022-11-29 13:09:30 -0600

    create a hash of store items

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+item1 = {name: "shoes", color: "green", price: 60}
+p item1
+
+item2 = {:name => "book", :color => "yellow", :price => 17}
+p item2
+
+item3 = {:name => "lamp", :color => "white", :price => 25}
+p item3
