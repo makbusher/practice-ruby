3ed40e93d9af7efcbde2e35135c761f19297d6b0
Makayla <makaylabusher@gmail.com>
2022-11-29 13:40:49 -0600

    Added arrays for items

--- store_item.rb
+++ store_item.rb
@@ -6,3 +6,16 @@ p item2
 
 item3 = {:name => "lamp", :color => "white", :price => 25}
 p item3
+
+item1 = ["shoes", "green", 60]
+item2 = ["book", "yellow", 17]
+item3 = ["lamp", "white", 25]
+
+class Items
+  def initialize(input_name, input_color, input_price)
+    @name = input_name
+    @color = input_color
+    @price = input_price
+  end 
+
+  def 
