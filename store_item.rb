b6affbcff2c8ddb1daedcf70d7259a7525ba2cdb
Makayla <makaylabusher@gmail.com>
2022-11-29 14:03:54 -0600

    add reader/writer methods for a class of items

--- store_item.rb
+++ store_item.rb
@@ -1,21 +1,46 @@
 item1 = {name: "shoes", color: "green", price: 60}
-p item1
+
 
 item2 = {:name => "book", :color => "yellow", :price => 17}
-p item2
+
 
 item3 = {:name => "lamp", :color => "white", :price => 25}
-p item3
+
 
 item1 = ["shoes", "green", 60]
 item2 = ["book", "yellow", 17]
 item3 = ["lamp", "white", 25]
 
-class Items
+class Item
   def initialize(input_name, input_color, input_price)
     @name = input_name
     @color = input_color
     @price = input_price
   end 
 
-  def 
+  def name
+    @name
+  end
+
+  def name=(input_name)
+    @name = input_name
+  end
+
+  def name
+    @name 
+  end
+
+  def color
+    @color
+  end
+
+  def price 
+    @price
+  end
+end
+
+  item1 = Item.new("shoes", "green", 60)
+  p item1
+  item2 = Item.new("book", "yellow", 17)
+  p item2.name
+  p item2.name=("shoes")
