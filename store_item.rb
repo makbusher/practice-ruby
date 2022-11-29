a975047d1be5d615407816d35a85360e075828fb
Makayla <makaylabusher@gmail.com>
2022-11-29 15:02:46 -0600

    add options hash

--- store_item.rb
+++ store_item.rb
@@ -12,11 +12,20 @@ item2 = ["book", "yellow", 17]
 item3 = ["lamp", "white", 25]
 
 class Item
-  def initialize(input_name, input_color, input_price)
-    @name = input_name
-    @color = input_color
-    @price = input_price
-  end 
+
+attr_reader: input_name, input_color, input_price
+attr_writer: input_name
+
+  # def initialize(input_name, input_color, input_price)
+  #   @name = input_name
+  #   @color = input_color
+  #   @price = input_price
+  # end 
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
+  end
 
   def name
     @name
@@ -39,8 +48,13 @@ class Item
   end
 end
 
+
   item1 = Item.new("shoes", "green", 60)
   p item1
   item2 = Item.new("book", "yellow", 17)
   p item2.name
-  p item2.name=("shoes")
+  item2.name=("shirt")
+  p item2
+
+  item1 = Item.new(name: "shoes", color: "green", price: 60)
+  p item1
